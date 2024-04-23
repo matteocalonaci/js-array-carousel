84eb06795c449523a74c1367542b41f19b8fbcb1
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-04-23 18:33:39 +0200

    M1 - creo Layout

---
+++ JS/main.js
@@ -0,0 +1,41 @@
+//  MILESTONE 1
+// Per prima cosa, creiamo il markup statico come visto in mattinata.
+// Costruiamo il container e inseriamo le immagini.
+// Solo una sarà visibile grazie a una sua classe, avremo così la struttura base.
+// Condite con un pò di CSS per dare stile(senza esagerare).
+// Vi consiglio di rispolvere object - fit
+
+
+// MILESTONE 2
+// Ora dobbiamo scrivere del codice
+// che al caricamento della pagina sposti la slide attiva da quella corrente alla successiva,
+// non pensate al click.
+// Devo recuperare un elemento specifico ? Devo recuperare più elementi ?
+// Aiutandovi con la console dell'ispettore recuperate tutti gli elementi necessari.
+// Scrivete poi il codice che all'avvio della pagina in automatico sposti
+// la slide attiva dalla prima alla seconda.
+
+
+// MILESTONE 3
+// Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva,
+// Se abbiamo lavorato correttamente in milestone 2 dovrebbe bastarci poco.
+// Creiamo un event listener per gestire il click sul pulsante.
+// All'interno spostiamo il codice scritto in milestone 2 per far si che
+//  la slide non venga cambia al caricamento della pagina, ma al nostro click sul pulsante.
+// Se superate l'ultima slide si spacca tutto e non fa nulla: è un bonus.
+
+
+// MILESTONE 4
+// Create un ulteriore pulsante che vi permetta di fare il lavoro inverso:
+//  la slide attiva invece di andare avanti torna indietro.
+// Potete partire copiando il codice ma cosa dovete cambiare ?
+//  In questa milestone vi assicurerete di aver davvero capito la precedente
+
+
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,24 @@
+
+
+MILESTONE 1
+- creare il titolo dell'esercizio
+- inserire le immagini
+- creare i bottoni
+- applicare il display none e l'active (css e html)
+- applicare object-fit  (css e html)
+- applicare qualche regola CSS per stilizzare la pagina 
+
+
+MILESTONE 2
+- recupero tutte le slide e le metto in un array
+- fare un ciclo su tutte le slide per trovare quella con l'active
+- rimuovo l'active e lo assegno al successivo e così via
+- sposto l'active da i=0 a i=1
+
+
+MILESTONE 3
+- creare un addEventListner
+- assegnare a un bottone la funzione di andare avanti 
+
+MILESTONE 4
+- assegnare al secondo bottone la funzione di andare indietro 
